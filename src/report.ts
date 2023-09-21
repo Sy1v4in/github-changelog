15b819492dc5f73b8fa1983d344be8e9385fd51f
sylvain <fraisse.sylvain@gmail.com>
2023-09-21 08:33:33 +0200

    :lipstick: improve the slack list report

--- src/report.ts
+++ src/report.ts
@@ -2,8 +2,8 @@ import { Eta } from 'eta'
 import { PullRequest, Repository } from './github.ts'
 
 const STYLES = {
-  markdown: '*<%= it.repository.repo %>*\n<% it.pullRequests.forEach(pr => { %>• [[#<%= pr.number %>](<%= pr.url %>)] <%= pr.title %> \n<%}) %>',
-  slack: '*<%= it.repository.repo %>*\n<% it.pullRequests.forEach(pr => { %>  - [<<%= pr.url %> | #<%= pr.number %>>] <%= pr.title %> \n<%}) %>'
+  markdown: '*<%= it.repository.repo %>*\n<% it.pullRequests.forEach(pr => { %> • [[#<%= pr.number %>](<%= pr.url %>)] <%= pr.title %> \n<%}) %>',
+  slack: '*<%= it.repository.repo %>*\n<% it.pullRequests.forEach(pr => { %> • [<<%= pr.url %> | #<%= pr.number %>>] <%= pr.title %> \n<%}) %>'
 }
 
 export type ReportStyle = keyof typeof STYLES
